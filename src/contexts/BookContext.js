dd7031d6f871f5a2255bd5a1e85314a397fd8c8b
Alex Panas <alexpanasucla@gmail.com>
2019-07-03 20:16:33 +0300

    add localstorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,16 +1,25 @@
 // Create context to share with app's components 
 
-import React,{createContext,useReducer} from 'react'
+import React,{createContext,useReducer,useEffect} from 'react'
 import {BookReducer} from '../reducers/BookReducer'
 
 // Create and export BookContext with info on Books 
 
 export const BookContext = createContext();
 
-
+// Custom hook to enable use Reducer
 const BookContextProvider=(props) =>{
+    
 
-    const [books, dispatch] = useReducer(BookReducer,[])
+    const [books, dispatch] = useReducer(BookReducer,[],
+        ()=>{
+            const localData = localStorage.getItem('books');
+            return localData?JSON.parse(localData):[]
+        })
+
+    useEffect(() => {
+        localStorage.setItem('books',JSON.stringify(books))
+    }, [books])
 
 
     return (
